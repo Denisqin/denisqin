4c866d55dd212ae5b183bf0647dfba89390af37a
Denisqin <deniskin9803@gmail.com>
2017-07-18 15:05:35 +0300

    Started

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+function top($title)
+{
+	echo "
+		<html>
+		<head>
+			<meta charset='utf-8';
+			<title>".$title."</title>
+			<link rel='stylesheet' type='text/css' href='css/style.css'>
+		</head>
+		<body>
+
+	";
+}
+function bottom()
+{
+	echo "
+		</body>
+		</html>
+	"
+}
+?>
