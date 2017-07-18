2893d45cf500c8577d7b0935af0af9bd15a9da81
Denisqin <deniskin9803@gmail.com>
2017-07-18 19:10:13 +0300

    19:06 18.07.2017 Update 0.1

--- config.php
+++ config.php
@@ -2,21 +2,23 @@
 function top($title)
 {
 	echo "
+		<!DOCTYPE html>
 		<html>
 		<head>
-			<meta charset='utf-8';
+			<meta charset='utf-8'>
 			<title>".$title."</title>
 			<link rel='stylesheet' type='text/css' href='css/style.css'>
 		</head>
 		<body>
 
 	";
-}
+};
 function bottom()
 {
 	echo "
 		</body>
 		</html>
-	"
-}
+	";
+};
+
 ?>
