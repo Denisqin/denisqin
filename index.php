6f90c5b22d02976e27ed17cbdcdaeb9439b644bd
Denisqin <deniskin9803@gmail.com>
2017-07-18 19:10:24 +0300

    19:06 18.07.2017 Update 0.1

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
+ob_start();
 include 'config.php';
-top('Ggg');
+top("forYou");
+echo "
 
+	<section>
+		<h1>Welcome to page for you</h1>
+		<h3>Enter you're invite code</h3>
+		<form method='post' action=''>
+			<input type='text' name='inviteCode'>
+			<br>
+			<input type='submit' name='inviteCodeSubmit'>
+		</form>
+	</section>
+
+
+
+";
 bottom();
+ob_flush();
 ?>
